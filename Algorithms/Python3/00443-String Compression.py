375ecd03fa78aa0acf8eb9f8bfbe296fc9de3a82
David Huang <david.huang3214@gmail.com>
2024-05-13 18:56:06 -0700

    5/13/2024

---
+++ Algorithms/Python3/00443-String Compression.py
@@ -0,0 +1,46 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        index = 0
+        count = 1
+
+        for i in range(len(chars)):
+            curr = chars[i]
+
+            if i < len(chars)-1:
+                if chars[i] == chars[i+1]:
+                    count += 1
+                else:
+                    chars[index] = curr
+                    index += 1
+                    digits = []
+                    if count > 1:
+                        # add digits in place
+                        while count > 0:
+                            digits.insert(0, count % 10)
+                            count = count // 10
+                    for n in digits:
+                        chars[index] = str(n)
+                        index += 1
+                    count = 1   # count gets reset here
+            else:
+                chars[index] = curr
+                index += 1
+                digits = []
+                if count > 1:
+                    # add digits in place
+                    while count > 0:
+                        digits.insert(0, count % 10)
+                        count = count // 10
+                for n in digits:
+                    chars[index] = str(n)
+                    index += 1
+
+        return index
+                
+'''
+    keep 2 pointers (one for index, one for iterating)
+
+    LOL i can't believe it worked!!
+
+    
+'''
