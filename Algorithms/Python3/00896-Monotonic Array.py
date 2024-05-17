ae40ebde60265a39369967816cdffff6acc6c58d
David Huang <david.huang3214@gmail.com>
2024-05-17 14:28:16 -0700

    5/17/2024

---
+++ Algorithms/Python3/00896-Monotonic Array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        copy = nums[::1]
+        copy.sort(reverse=True)
+        if(copy == nums):
+            return True
+        copy.sort()
+        if(copy == nums):
+            return True
+
+        return False
+
+
+'''
+sort array desc, check if equals to original
+sort array asc, check if equals to original
+
+if not either, then return False
+
+'''
